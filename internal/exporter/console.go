8dfb527211b7f5509590e139ee7d50b4d9587d60
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-08-06 06:28:47 -0700

    Create console.go

---
+++ internal/exporter/console.go
@@ -0,0 +1,14 @@
+package exporter
+
+import (
+	"fmt"
+
+	"github.com/username/omniping/pkg/telemetry"
+)
+
+func PrintResults(results []telemetry.Result) {
+	for _, r := range results {
+		fmt.Printf("[%s] Target: %-15s Layer: %s Probe: %s Status: %s Latency: %v Error: %s\n",
+			r.Timestamp.Format("15:04:05"), r.Target, r.Layer, r.Probe, r.Status, r.Latency, r.Error)
+	}
+}
