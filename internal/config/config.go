d08b2ea4f64fc9f812e87e7a9a1006cee9166fe7
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-08-06 06:26:44 -0700

    Create config.go

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+ `yaml:"targets"`
+}
+
+type TargetConfig struct {
+	Name    string `yaml:"name"`
+	Address string `yaml:"address"`
+	Layer   string `yaml:"layer"`
+	Type    string `yaml:"type"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
